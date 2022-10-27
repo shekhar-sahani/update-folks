961151f479d6f8bdc8d53c3571aa90cd5b43eeca
@Starlord0987 <shekharsahani0987@gmail.com>
2022-10-27 15:30:29 +0530

    Added google console code

--- pages/anime-details/[id]/[slug].js
+++ pages/anime-details/[id]/[slug].js
@@ -15,6 +15,7 @@ export default function AnimeDetail() {
     <>
     <Head>
         <title>{item?.title} </title>
+        <meta name="google-site-verification" content="pfzk24IbYS53h7fP868UIjlzjtUcIKxzeONE1BJPD8c" />
         <meta name="description" content={item?.desc} />
         <meta name="keyword" content="upcoming anime, upcoming anime 2023, upcoming anime january, new and upcoming anime, anime"></meta>
         <meta name="author" content="updatefolks" ></meta>
--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,7 @@ export default function Home() {
     <div className={styles.container}>
       <Head>
         <title>Upcoming Anime 2023 </title>
+        <meta name="google-site-verification" content="pfzk24IbYS53h7fP868UIjlzjtUcIKxzeONE1BJPD8c" />
         <meta name="description" content="upcoming anime 2023 | anime airing in january 2023" />
         <meta name="keyword" content="upcoming anime, upcoming anime 2023, upcoming anime january, new and upcoming anime, anime"></meta>
         <meta name="author" content="updatefolks" ></meta>
