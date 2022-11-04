bb12ab02eb4367fe0f2de7039ece442cd770f6f7
@Starlord0987 <shekharsahani0987@gmail.com>
2022-11-04 13:10:28 +0530

    A

--- pages/anime/best/2022.js
+++ pages/anime/best/2022.js
@@ -3,6 +3,17 @@ import BestAnime2022 from '../../../components/BestAnime2022'
 
 export default function BestAnime() {
   return (
+    <>
+    <Head>
+        <title>Anime Spring 2023 - Update Folks</title>
+        <meta name="google-site-verification" content="pfzk24IbYS53h7fP868UIjlzjtUcIKxzeONE1BJPD8c" />
+        <meta name="description" content="upcoming anime 2023 | anime airing in spring 2023" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+        <meta name="keyword" content="upcoming anime,  anime spring 2023,  anime releasing in spring, upcoming best anime starting this next year"></meta>
+        <meta name="author" content="updatefolks" ></meta>
+        <link rel="icon" href="/starlord.png" />
+      </Head>
     <BestAnime2022 />
+    </>
   )
 }
--- pages/anime/winter/2023.js
+++ pages/anime/winter/2023.js
@@ -3,6 +3,17 @@ import AnimeWinter2023 from '../../../components/AnimeWinter2023'
 
 export default function AnimeSpring() {
   return (
+    <>
+    <Head>
+        <title>Anime Winter 2023 - Update Folks</title>
+        <meta name="google-site-verification" content="pfzk24IbYS53h7fP868UIjlzjtUcIKxzeONE1BJPD8c" />
+        <meta name="description" content="upcoming anime 2023 | anime airing in winter 2023" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+        <meta name="keyword" content="upcoming anime,  anime winter 2023,  anime january, upcoming best anime starting this next year"></meta>
+        <meta name="author" content="updatefolks" ></meta>
+        <link rel="icon" href="/starlord.png" />
+      </Head>
     <AnimeWinter2023 />
+    </>
   )
 }
